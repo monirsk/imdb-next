5515dcb8c9d434ab855d46b7a04b2838cf3be5a6
Monir <kmsehab130@gmail.com>
2024-03-23 14:59:40 +0600

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
+        consequuntur voluptate nulla dicta unde fugiat provident rem hic ratione
+        possimus! Laudantium natus, veritatis neque nobis quod fugiat delectus
+        doloremque esse?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
+        expedita, quod vero praesentium sapiente deserunt tempora itaque
+        possimus excepturi dolorem. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Sit, quam. Quae dolores qui fugit illum! Sint fugit
+        non perferendis totam quam nostrum? Architecto, provident vel
+        repellendus id dicta qui quas ducimus harum consectetur minima quam
+        error reprehenderit nihil nemo itaque nostrum porro? Iure rerum
+        deleniti, eligendi quidem accusantium quam assumenda?
+      </p>
+    </div>
+  );
+}
